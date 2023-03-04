433e7d2bc7ff58581ba5d0832f268152b4a08691
hyongti <hyongtiii@gmail.com>
2023-03-04 13:23:02 +0900

    Deploy

---
+++ _next/static/7M9Xvq5UisWJ5PZphCvLk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-595cf52de49b45e5.js"],"/_error":["static/chunks/pages/_error-19e73c3be68140bc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
