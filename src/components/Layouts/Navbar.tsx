d5c45f698a2885df46f0fc5db1f26a6dcac257e5
hyongti <dev.hyongti@gmail.com>
2023-05-20 23:15:51 +0900

    useMediaQuery 훅 추가

--- src/components/Layouts/Navbar.tsx
+++ src/components/Layouts/Navbar.tsx
@@ -1,9 +1,12 @@
-import { ChevronDownIcon } from "@heroicons/react/24/solid";
-import Link from "next/link";
-import { useState } from "react";
-import Navlinks from "./Navlinks";
+import { ChevronDownIcon } from '@heroicons/react/24/solid';
+import Link from 'next/link';
+import { useState } from 'react';
+import Navlinks from './Navlinks';
+import useIsBreakPoint from 'hooks/useIsBreakPoint';
 
 const Navbar = () => {
+  const isMobile = useIsBreakPoint();
+
   const [open, setOpen] = useState(false);
   return (
     // TODO: 애니메이션 개선 필요
@@ -16,7 +19,7 @@ const Navbar = () => {
           <button onClick={() => setOpen(!open)}>
             <ChevronDownIcon
               className={`w-4 h-4 text-black ${
-                open ? "-rotate-180" : "rotate-0"
+                open ? '-rotate-180' : 'rotate-0'
               } transition-transform`}
             />
           </button>
---
+++ src/hooks/useIsBreakPoint.ts
@@ -0,0 +1,5 @@
+import useMediaQuery from './useMediaQuery';
+
+const useIsBreakPoint = () => useMediaQuery({ query: '(max-width: 679px)' });
+
+export default useIsBreakPoint;
